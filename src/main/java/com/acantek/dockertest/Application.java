fd9c1256d5dfcb4d7765392ea054caebcf1b5841
Viktar Lebedzeu <viktar.lebedzeu@itrexgroup.com>
2019-04-25 14:43:23 +0300

    Initial Spring application files

---
+++ src/main/java/com/acantek/dockertest/Application.java
@@ -0,0 +1,14 @@
+package com.acantek.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Viktar Lebedzeu
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
