62fdcfc03fca68221c4d83450eaf2399bba19d7f
Viktar Lebedzeu <viktar.lebedzeu@itrexgroup.com>
2019-04-29 13:53:37 +0300

    Created 2 child modules

--- server/src/main/java/com/acantek/dockertest/dockertest/Application.java
+++ server/src/main/java/com/acantek/dockertest/dockertest/Application.java
@@ -1,4 +1,4 @@
-package com.acantek.dockertest;
+package com.acantek.dockertest.dockertest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/src/main/java/com/acantek/dockertest/dockertest/controller/ApplicationInfoController.java
+++ server/src/main/java/com/acantek/dockertest/dockertest/controller/ApplicationInfoController.java
@@ -1,4 +1,4 @@
-package com.acantek.dockertest.controller;
+package com.acantek.dockertest.dockertest.controller;
 
 import com.acantek.dockertest.dto.ApplicationInfo;
 import org.slf4j.Logger;
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
