1e9a8fc104b3c203fd7d093e07311c41f745f789
Viktar Lebedzeu <viktar.lebedzeu@itrexgroup.com>
2019-04-30 18:33:38 +0300

    Configured connection between Eureka NS server and clients. Configured Docker Compose file

--- server/src/main/java/com/acantek/dockertest/server/ServerApplication.java
+++ server/src/main/java/com/acantek/dockertest/server/ServerApplication.java
@@ -2,19 +2,21 @@ package com.acantek.dockertest.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author Viktar Lebedzeu
  */
 @SpringBootApplication(scanBasePackages = "com.acantek.dockertest")
-@EnableEurekaClient
-/*
 @EnableDiscoveryClient
-@EnableHystrix
+@EnableZuulProxy
 @EnableHystrixDashboard
 @EnableCircuitBreaker
-*/
+@EnableTurbine
 public class ServerApplication {
     public static void main(String... args) {
         SpringApplication.run(ServerApplication.class, args);
