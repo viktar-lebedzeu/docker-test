0830f2111a5f0cdac9ca3f68893a88283e5a7dd2
Viktar Lebedzeu <viktar.lebedzeu@itrexgroup.com>
2019-05-27 18:24:35 +0300

    Configured connection to git repository for Spring config server module

--- server/src/main/java/com/acantek/dockertest/server/controller/ApplicationInfoController.java
+++ server/src/main/java/com/acantek/dockertest/server/controller/ApplicationInfoController.java
@@ -1,11 +1,14 @@
 package com.acantek.dockertest.server.controller;
 
 import com.acantek.dockertest.common.dto.ApplicationInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +21,19 @@ import java.util.List;
 /**
  * @author Viktar Lebedzeu
  */
+@RefreshScope
 @RestController
 @RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@Slf4j
 public class ApplicationInfoController {
-    /** Logger */
-    private static final Logger logger = LoggerFactory.getLogger(ApplicationInfoController.class);
-
     private final ApplicationInfo applicationInfo;
     private final DiscoveryClient discoveryClient;
+
+    @Value("${application.descriptionMessage}")
+    private String appDescriptionMessage;
+
+    @Value("${eureka.client.proxyHost}")
+    private String eurekaClientHost;
 
     @Autowired
     public ApplicationInfoController(ApplicationInfo applicationInfo, DiscoveryClient discoveryClient) {
@@ -35,6 +43,8 @@ public class ApplicationInfoController {
 
     @GetMapping("/info")
     public ResponseEntity<ApplicationInfo> getApplicationInfo() {
+        log.info("App description    : {}", appDescriptionMessage);
+        log.info("eureka client host : {}", eurekaClientHost);
         final List<ServiceInstance> clients = discoveryClient.getInstances("docker-test-entry");
         applicationInfo.setServices(clients);
         return new ResponseEntity<>(applicationInfo, HttpStatus.OK);
